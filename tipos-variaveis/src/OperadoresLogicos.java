9e9f0c06f65e8a7c265485e6409fb20de7175123
https-luana <zusumi621@gmail.com>
2023-03-10 18:57:38 -0300

    Operadores Logicos

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,17 @@
+public class OperadoresLogicos {
+    public static void main(String[] args){
+
+        boolean condicao1 =true;
+
+        boolean condicao2 = true;
+        
+        if(condicao1 && (7 > 4 )){
+            System.out.println("Uma das duas condições é verdadeiras");}
+
+        if(condicao1 || condicao2){
+            System.out.println("As duas condiçoes são verdadeiras");
+        }
+        System.out.println("Fim");
+        
+    }
+}
--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -1,4 +1,4 @@
-public class operadores {
+public class Operadores {
     public static void main(String[] args){
 
         String nomeUm = "Luana";
